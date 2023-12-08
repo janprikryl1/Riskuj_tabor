2ab52445bf3af4e8dded4b40b8a9c797463a6663
janprikryl1 <honza.prikryl.hp@gmail.com>
2023-12-08 18:42:10 +0100

    Doplněny všechny odpovědi a odpočet

--- src/Groups.js
+++ src/Groups.js
@@ -1,22 +1,28 @@
 function Groups(props) {
-  return (
-   <div className="container" style={{paddingTop:"10px"}}>
-       <div className="row d-flex justify-content-center">
-               <div className="col-sm-2">
-                   <h3 style={{color:"blue"}}>Skupinka 1: {props.points1}</h3>
-               </div>
-               <div className="col-sm-2">
-                   <h3 style={{color:"green"}}>Skupinka 2: {props.points2}</h3>
-               </div>
-               <div className="col-sm-2">
-                   <h3 style={{color:"red"}}>Skupinka 3: {props.points3}</h3>
-               </div>
-               <div className="col-sm-2">
-                   <h3 style={{color:"orange"}}>Skupinka 4: {props.points4}</h3>
-               </div>
-       </div>
-   </div>
-  );
+    // Vytvořte pole objektů s informacemi o skupinkách a bodech
+    const groupsData = [
+        { name: "Skupinka 1", points: props.points1, color: "blue" },
+        { name: "Skupinka 2", points: props.points2, color: "green" },
+        { name: "Skupinka 3", points: props.points3, color: "red" },
+        { name: "Skupinka 4", points: props.points4, color: "orange" },
+    ];
+
+    // Seřaďte pole objektů podle počtu bodů sestupně
+    const sortedGroups = groupsData.sort((a, b) => b.points - a.points);
+
+    return (
+        <div className="container" style={{ paddingTop: "10px" }}>
+            <div className="row d-flex justify-content-center">
+                {sortedGroups.map((group, index) => (
+                    <div key={index} className="col-sm-2">
+                        <h3 style={{ color: group.color }}>
+                            {group.name}: {group.points}
+                        </h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Groups;
--- src/QuestionModal.js
+++ src/QuestionModal.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Parser from 'html-react-parser';
 
 function QuestionModal(props) {
+    const [countdown, setCountdown] = useState(60); // Počáteční hodnota odpočtu v sekundách
+
+    useEffect(() => {
+        let timer;
+
+        if (countdown > 0) {
+            timer = setInterval(() => {
+                setCountdown(prevCountdown => prevCountdown - 1);
+            }, 1000);
+        }
+
+        // Zastavení odpočtu a vyčištění intervalu po zavření modálního okna
+        return () => {
+            clearInterval(timer);
+        };
+    }, [countdown]);
+
+    const resetCountdown = () => {
+        setCountdown(60);
+    };
+
     return (
         <Modal
             {...props}
@@ -11,6 +33,7 @@ function QuestionModal(props) {
             centered
             backdrop="static"
             keyboard={false}
+            onShow={resetCountdown} // Přidáno onShow událost, která se volá při zobrazení modálního okna
         >
             <Modal.Header>
                 <Modal.Title id="contained-modal-title-vcenter">
@@ -18,9 +41,14 @@ function QuestionModal(props) {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h1>{props.text}</h1>
-
-
+                <div className="row">
+                    <div className="col-sm-10">
+                        <h1>{Parser(props.text)}</h1>
+                    </div>
+                    <div className="col-sm-1">
+                        {countdown < 11 ? <h3 style={{paddingTop:"10px", color:"red", textAlign:"center"}}>Odpočet: {countdown}</h3> : <h3 style={{paddingTop:"10px", textAlign:"center"}}>Odpočet: {countdown}</h3>}
+                    </div>
+                </div>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="primary" onClick={props.onHide1}>
@@ -40,6 +68,7 @@ function QuestionModal(props) {
                 </Button>
             </Modal.Footer>
         </Modal>
-    )
+    );
 }
+
 export default QuestionModal;
